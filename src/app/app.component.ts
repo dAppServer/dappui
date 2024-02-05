44f4f5eed39618f7c5e367f4af58e74ce7171d7c
Snider <snider@lethean.io>
2024-02-05 00:21:09 +0000

    adds angular sdk package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { APP_MENU_ITEMS } from './app.menu';
+import { serverSDKApiModule, SystemService } from '@lethean/api-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, serverSDKApiModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dappui';
   nav = APP_MENU_ITEMS
+
+  constructor(private systemService: SystemService) {
+    this.systemService.checkServer().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
