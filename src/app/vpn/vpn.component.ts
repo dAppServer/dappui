2382eff712ce44d1d9e45f181d66bfd52b98bab4
Snider <snider@lethean.io>
2024-03-18 16:53:54 +0000

    adds current gui version

---
+++ src/app/vpn/vpn.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-vpn',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './vpn.component.html',
+  styleUrl: './vpn.component.scss'
+})
+export class VpnComponent {
+
+}
